0d2890af7907d86000c4b1d36fa3b543c9199c44
Ubaid196 <uurrehmansiddiqui@gmail.com>
2023-02-25 04:00:01 +0500

    Words Counter CLI

---
+++ README.md
@@ -0,0 +1,6 @@
+# Words Counter
+This is words counter project using typescript node js.
+
+To install the package, run the following command:
+
+    npm i @ubaid196/project_wordcounter
---
+++ bin/index.js
@@ -0,0 +1,53 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import showBanner from "node-banner";
+async function myBanner() {
+    await showBanner("\nWords Counter", chalk.blue("Welcome!"), "green");
+}
+await myBanner();
+async function countWords() {
+    let userInput = await inquirer.prompt([
+        {
+            name: "enterPara",
+            type: "string",
+            message: "Enter the text/Paragraph: ",
+        },
+    ]);
+    const { enterPara } = userInput;
+    function wordsCount() {
+        let words_Counter = enterPara.split(" ").filter((words) => words !== "");
+        console.log(chalk.bgCyanBright(`Total words: ${words_Counter.length}`));
+    }
+    wordsCount();
+    function characCount() {
+        let strAdd = "";
+        let i = 0;
+        while (i < enterPara.length) {
+            if (enterPara[i] != " ") {
+                strAdd = strAdd + enterPara[i];
+            }
+            i++;
+        }
+        console.log(`Total Character: ${strAdd.length}`);
+    }
+    characCount();
+    startAgain();
+}
+async function startAgain() {
+    let againStart = await inquirer.prompt({
+        type: "list",
+        name: "restartAgain",
+        choices: [`Yes`, `No`],
+        message: chalk.bgBlue("\nDo you want to continue? "),
+    });
+    let user_Reqd = againStart.restartAgain;
+    if (user_Reqd === `Yes`) {
+        console.clear();
+        countWords();
+    }
+    else if (user_Reqd === `No`) {
+        console.log(chalk.blueBright(`~~~~~~~~~~~~THANK YOU FOR USING CURRENCY EXCHANGE APP~~~~~~~~~~~~`));
+    }
+}
+countWords();
